38b304174314275567608843ead8ebc1aa70c6b3
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-12 14:26:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import AnimatedContainer from "./components/AnimatedContainer";
+
 export default function Home() {
+
   return (
-    <div>
-      <p>Webseite</p>
-    </div>
+    <AnimatedContainer>
+      <Link href="/">Wie funktioniert das?</Link>
+      <Link href="/raid-generator/home">Time</Link>
+    </AnimatedContainer>
   );
 }
