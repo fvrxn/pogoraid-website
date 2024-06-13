fc138c5d95738974e1d9c16a73c35e35fbf0d889
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-14 01:55:15 +0200

    update tailwind config
    added font styles, colors & added font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      'heading': ['40px', {
+        lineHeight: '48px',
+      }],
+      'button': ['16px', {
+        lineHeight: '24px',
+      }],
+      'footer': ['10px', {
+        lineHeight: '15px',
+      }],
+      'text': ['14px', {
+        lineHeight: '20px',
+      }],},
     extend: {
+      colors: {
+        white: "#F5F5F5",
+        black: "#171717",
+        primary: "#16A34A",
+      },
+      fontFamily: {
+        'Nunito': ['Nunito', 'sans-serif'],
+      },
     },
   },
   plugins: [],
