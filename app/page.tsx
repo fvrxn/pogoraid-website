b195f2863a2f1aee90c9734e9174c121f372bb14
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-13 21:20:14 +0200

    created how-to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import AnimatedContainer from "./components/AnimatedContainer";
+import Card from "./components/Card";
 
 export default function Home() {
 
   return (
     <AnimatedContainer>
-      <Link href="/">Wie funktioniert das?</Link>
-      <Link href="/raid-generator/home">Time</Link>
+      <Card>
+        <h1>Willkommen beim Raid Generator!</h1>
+        <Link href="/">Wie funktioniert das?</Link>
+        <Link href="/raid-generator/home">Time</Link>
+      </Card>
     </AnimatedContainer>
   );
 }
