a878af4afef6fedb9d36db6169ab09d43f996d28
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-13 11:33:02 +0200

    added spee insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <div>
             {children}
             <Analytics/>
+            <SpeedInsights/>
         </div>
       </body>
     </html>
