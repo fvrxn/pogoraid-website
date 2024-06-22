e0e36ef8a89ad6b92d3d12bf5d210e6d5817524d
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-22 10:07:26 +0200

    created raidboss page

--- app/raid-generator/raidboss/page.tsx
+++ app/raid-generator/raidboss/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+
+import BackButton from "@/app/components/BackButton";
+import Button from "@/app/components/Button";
+import DropDown from "@/app/components/DropDown";
+
+
 export default function RaidBoss() {
     return (
-      <div>
-        <p>RaidBoss</p>
+      <div className="flex flex-col h-screen">
+          <BackButton/>
+          <h1 className="font-Nunito text-white font-semibold text-heading mt-6">
+            Welchen Raidboss möchtest du hosten?
+          </h1>
+        <DropDown text="Raidboss auswählen"/>
+        <div id="spacer" className="mt-40"/>
+        <Button linkTo="/raid-generator/range" text="Weiter"/>
       </div>
     );
   }
