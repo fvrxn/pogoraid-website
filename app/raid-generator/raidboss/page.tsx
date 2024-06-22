89c1a850cd9ad386e137c83fad6943dea4803349
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-22 10:09:27 +0200

    cleanup code

--- app/raid-generator/raidboss/page.tsx
+++ app/raid-generator/raidboss/page.tsx
@@ -1,20 +1,21 @@
-'use client'
+"use client";
 
+// components imports
 import BackButton from "@/app/components/BackButton";
 import Button from "@/app/components/Button";
 import DropDown from "@/app/components/DropDown";
 
 
 export default function RaidBoss() {
-    return (
-      <div className="flex flex-col h-screen">
-          <BackButton/>
-          <h1 className="font-Nunito text-white font-semibold text-heading mt-6">
-            Welchen Raidboss möchtest du hosten?
-          </h1>
-        <DropDown text="Raidboss auswählen"/>
-        <div id="spacer" className="mt-40"/>
-        <Button linkTo="/raid-generator/range" text="Weiter"/>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col">
+      <BackButton />
+      <h1 className="font-Nunito text-white font-semibold text-heading mt-6">
+        Welchen Raidboss möchtest du hosten?
+      </h1>
+      <DropDown text="Raidboss auswählen" />
+      <div id="spacer" className="mt-40" />
+      <Button linkTo="/raid-generator/range" text="Weiter" />
+    </div>
+  );
+}
