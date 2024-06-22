2d5e9dc02845a1e2d8a5f37971dcc8490a1bba2d
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-06-22 10:22:27 +0200

    update time page
    - implementation of time inputs

--- app/raid-generator/time/page.tsx
+++ app/raid-generator/time/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+// components imports
+import BackButton from "@/app/components/BackButton";
+import Button from "@/app/components/Button";
+import TimeInput from "@/app/components/TimeInput";
+
+
 export default function Time() {
     return (
-      <div>
-        <p>Time</p>
-      </div>
+      <div className="flex flex-col">
+      <BackButton />
+      <h1 className="font-Nunito text-white font-semibold text-heading mt-6">
+        Wann findet der Raid statt?
+      </h1>
+      <TimeInput />
+      <div id="spacer" className="mt-40" />
+      <Button linkTo="/raid-generator/time" text="Weiter" />
+    </div>
     );
   }
