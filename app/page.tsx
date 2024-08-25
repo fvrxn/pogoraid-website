b2ba64a2b057ba2e179f1fa501ce9d7daab813b3
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-08-26 00:18:48 +0200

    style update & useeffect update

--- app/page.tsx
+++ app/page.tsx
@@ -15,36 +15,51 @@ import CheckBox from "./components/CheckBox";
 
 export default function Home() {
 
+  var unmount = true;
+  var availableData = false;
+
   const [raidBoss, setRaidBoss] = useState('');
   const [time, setTime] = useState('');
   const [trainer, setTrainer] = useState('');
   const [trainerCode, setTrainerCode] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
 
-  var unmount = true;
-
   useEffect(() => {
     if (unmount) {
-    // fetch trainer code & name
+    // fetch trainer name
     const savedTrainerName = localStorage.getItem('trainerName');
     if (savedTrainerName) {
+      // console log
       console.log('Trainer name: '+trainer+' found in local storage');
+      // change state
       setTrainer(savedTrainerName);
+      // display that there is data available
+      availableData = true;
     } else console.log('No trainer name found in local storage');
+
+    // fetch trainer code
     const savedTrainerCode = localStorage.getItem('trainerCode');
     if (savedTrainerCode) {
+      // console log
+      console.log('Trainer name: '+trainer+' found in local storage');
+      // change state
       setTrainerCode(savedTrainerCode);
+      // display that there is data available
+      availableData = true;
     } else console.log('No trainer code found in local storage');
+
+    // mounted
     unmount = false;
+    if (availableData) setRememberMe(true);
     }
   }, []);
   
 
   return (
     <AnimatedContainer>
-      <h1 className="text-white font-Nunito font-bold">Pokemon Go Raid Generator</h1>
+      <h1 className="text-white font-Nunito font-bold text-heading">Pokemon Go Raid Generator</h1>
 
-      <p className="text-white font-Nunito text-button">Welcome to the Pokemon Go Raid Generator. Please select the raid boss you would like to battle and the number of trainers in your group.</p>
+      <p className="text-white font-Nunito text-text pt-2">Welcome to the Pokemon Go Raid Generator. Please select the raid boss you would like to battle and the number of trainers in your group.</p>
 
 
       <div className="form">
@@ -57,7 +72,7 @@ export default function Home() {
 
         
         <Button linkTo="/result" text="Generate Raid" />
-        <CheckBox placeholder="Remember me" state={rememberMe} setState={setRememberMe} />
+        <CheckBox placeholder="Daten für nächsten Raid merken" state={rememberMe} setState={setRememberMe} />
 
 
 
