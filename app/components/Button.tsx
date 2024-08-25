8861cc3427dfdea3b886aa2c21ed8223e1311214
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-08-26 00:53:35 +0200

    add onclick prop

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link';
 interface ButtonProps {
     linkTo: string;
     text: string;
+    onClick: void;
 }
 
 
 
-export default function Button( {linkTo, text}: ButtonProps) {
+export default function Button( {linkTo, text, onClick}: ButtonProps) {
     return (
-        <div className="bg-primary rounded-lg w-full inline-flex text-center items-center justify-center px-8 py-4 cursor-pointer">
+        <div className="bg-primary rounded-lg w-full inline-flex text-center items-center justify-center px-8 py-4 cursor-pointer" onClick={() => {onClick}}>
             <Link className="text-white font-Nunito font-semibold text-button" href={linkTo}>{text}</Link>
         </div>
     );
