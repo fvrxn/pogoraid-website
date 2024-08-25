f0e94305dc6a7b1c9aa9135a25d69d399aa56b4d
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-08-25 23:43:55 +0200

    edit inputfield

--- app/components/InputField.tsx
+++ app/components/InputField.tsx
@@ -4,20 +4,23 @@ import { useState } from "react";
 
 // typescript types
 interface InputFieldProps {
+    type: string;
     placeholder: string;
     changeValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
     value: string;
 }
 
-export default function InputField({placeholder, changeValue, value}: InputFieldProps) {
+export default function InputField({type, placeholder, changeValue, value}: InputFieldProps) {
     return (
       <div className="flex flex-col">
         <input
             type="text"
+            id={type}
             className="border-2 border-white rounded-md p-2 font-Nunito text-button text-white bg-transparent mt-6"
             placeholder={placeholder}
             onChange={changeValue}
             value={value}
+            autoComplete="off"
         />
       </div>
     );
